6ec451ff356cc6c2647a76b8e0c54417005bba27
Bhagyashree-Chauhan <bhagyashreechauhan.11@gmail.com>
2026-08-16 15:55:26 +0400

    chore: add docker-compose for local Postgres

--- src/Wallet.Domain/Wallet.cs
+++ src/Wallet.Domain/Wallet.cs
@@ -4,13 +4,13 @@ public class Wallet
 {
     public Guid Id { get; private set; }
     public string Currency { get; private set; }
-    public DateTime CreatedAt { get; private set; }
+    public DateTimeOffset CreatedAt { get; private set; }
 
     public Wallet(Guid id, string currency)
     {
         Id = id;
         Currency = currency;
-        CreatedAt = DateTime.UtcNow;
+        CreatedAt = DateTimeOffset.UtcNow;
     }   
 
 }
